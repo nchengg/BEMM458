5a9a76ca273ba335dbdf8c4dc41d219219c841c8
nchengg <hfc206@exeter.ac.uk>
2025-09-26 20:09:15 +0800

    Week 1

---
+++ WORKSPACE/Session01/python_exercises1.py
@@ -0,0 +1,55 @@
+# Exercise 1
+
+name = "Nick"
+print (name)
+
+# Exercise 2
+
+x = 5
+y = 10
+print ("x = ", x, "y= ", y)
+
+
+# Exercise 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y) 
+
+# Exercise 4
+
+temp = "Hello"
+name = "Nick"
+print(temp + " " + name)
+
+# Exercise 5
+# Name printing
+
+# Exercise 6
+isSunny = True
+isWarm = False
+print(isSunny and isWarm)
+print(isSunny or isWarm)
+print(not isSunny)
+
+# Exercise 7
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are old enough to vote.")
+else:
+    print("You are not old enough to vote.")
+
+# Exercise 8
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a + b)
+
+if a > b:
+    print("The first number is greater.")
+elif a < b:
+    print("The second number is greater.")
+else:
+    print("Both numbers are equal.")
